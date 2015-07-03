dcf39dcca447423eb9cb5944f6320b27e320b3e6
PieterJanPauwels <pieterjanpauwels@gmail.com>
2015-07-03 15:52:10 +0200

    Added all the Klynt files and adjusted the index.html
    No changes made to existing files except for the index.html. Added
    script in head for player and change the iframe with a Vimeo film to
    player of Klynt.

---
+++ miniPlayerData.js
@@ -0,0 +1,13 @@
+klynt.miniPlayerData = {
+    "title": "Datawijs",
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": false,
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
+    "yesWording": "Yes",
+    "description": "Interactieve Video Voor Open Data",
+    "noWording": "No",
+    "url": "www.datawijs.be",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "analyticsKey": "",
+    "thanksForWatchingWording": "Thanks for watching!"
+}
